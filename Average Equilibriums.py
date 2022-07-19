a5c6b33bf265c409af075ffc5911a083b2de7772
Ayush Arora <egayush@gmail.com>
2022-07-19 12:49:47 -0400

    Averages of each column, with the first thrown out

--- Average Equilibriums.py
+++ Average Equilibriums.py
@@ -1,11 +1,9 @@
 import pandas as pd
-train_df = pd.read_csv('Data/SG_rmsdtph_train.csv')
-test_df = pd.read_csv('Data/SG_rmsdtph_test.csv')
-x_train = train_df.iloc[:,1:len(train_df.columns)-1].values.tolist()
-y_train = train_df.iloc[:,len(train_df.columns)-1].values
-x_test = test_df.iloc[:,1:len(train_df.columns)-1].values.tolist()
-y_test = test_df.iloc[:,len(train_df.columns)-1].values
-mean_y_train = y_train.mean()
-mean_y_test = y_test.mean()
-mean_y = (mean_y_train + mean_y_test) / 2
-print(mean_y)
+denoised_data = pd.read_csv('Data/SG_denoised_W99D5_rmsd-tph.csv')
+x_data = denoised_data.iloc[:,1:len(denoised_data.columns)-1].values
+y_data = denoised_data.iloc[:,len(denoised_data.columns)-1].values
+mean_ys = []
+for column in denoised_data:
+  eachColumn = denoised_data[column]
+  mean_ys.append(eachColumn.mean())
+mean_ys.pop(0)
