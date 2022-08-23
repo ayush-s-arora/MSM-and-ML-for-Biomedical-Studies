773d6e5985d786d46807e1f19fcf8611640215f9
Ayush Arora <egayush@gmail.com>
2022-08-22 19:14:29 -0500

    Definitions to pull dataframes into AllDataFxn

--- data_extraction_pw_hbond_2_anya.py
+++ data_extraction_pw_hbond_2_anya.py
@@ -1,29 +1,31 @@
-import pandas as pd
-import numpy as np
-import os
+def data_anya():
+    import pandas as pd
+    import numpy as np
+    import os
+    import math
+    from data_extraction_pw_hbond_anya import source1df
 
-folder_path = 'All Data/dl'
-temps = ['t3', 't20', 't37']
-phs = ['1', '2', '3', '4', '5']
-trials = ['_1', '_2']
-metric = 'sasa'
-#file_names = ['sasa01-26.xvg', 'sasa27-57.xvg', 'sasa01-16.xvg', 'sasa17-37.xvg', 'sasa38-57.xvg']
-#exception = '/t37/t37_1'
+    folder_path = 'Data'
+    temps = ['t3', 't3', 't3', 't3', 't3', 't20', 't20', 't20', 't20', 't37', 't37', 't37', 't37', 't37']
+    phs = ['1', '2', '3', '4', '5', '1', '3', '4', '5', '1', '2', '3', '4', '5']
+    metric = 'hbond'
+    include = 'p-w'
+    exclude2 = 'side'
+    # Call extracted data from source 1
+    source1df = source1df()
 
-
-all_data = []
-all_colnames = []
-for t in temps:
-    for p in phs:
-        for tr in trials:
+    all_data = []
+    all_colnames = []
+    for t in temps:
+        for p in phs:
             # Get into subfolder
-            cur_path = folder_path + '/' + t + '/' + p + tr
-            cur_column = t + '-' + p + tr
+            cur_path = folder_path + '/' + t + '-' + p
+            cur_column = t + '-' + p
             if os.path.exists(cur_path):
-                # Get all sasa files
+                # Get all hbond files, excluding hbond-p-w and hbond-side
                 file_names = []
                 for file in os.listdir(cur_path):
-                    if metric in file:
+                    if metric in file and include in file and not exclude2 in file:
                         file_names.append(file)
                 
                 # Extract numerical data from files
@@ -38,13 +40,13 @@ for t in temps:
                             vals = line.split()
                             nums = []
                             for i in range(len(vals)):
-                                if i == 0:
-                                    if '.500' in vals[i]:
-                                        nums.append(round(float(vals[i]) + 0.001,0))
+                                    if i == 0:
+                                        if '.050' in vals[i]:
+                                            nums.append(round(float(vals[i]) + 0.0001,1))
+                                        else:
+                                            nums.append(round(float(vals[i]),1))
                                     else:
-                                        nums.append(round(float(vals[i]),0))
-                                else:
-                                    nums.append(float(vals[i]))
+                                        nums.append(float(vals[i]))
                             cur_file_data.append(nums)
                     all_file_data.append(cur_file_data)
                 
@@ -85,7 +87,7 @@ for t in temps:
                     # Remove redundant time stamps
                     while len(cur_file_data) > 0 and cur_file_data[0][0] <= last_timestamp:
                         removed = cur_file_data.pop(0)
-                    if (cur_file_data[0][0] - last_timestamp) != 1:
+                    if not math.isclose(cur_file_data[0][0] - last_timestamp, 0.1):
                         print("Error: " + cur_column)
                         print(cur_file_data[0][0])
                         print(last_timestamp)
@@ -94,28 +96,41 @@ for t in temps:
                 
                 all_data.append(combined_list)
                 all_colnames.append(cur_column)
-# Add all data to a dataframe
-df = pd.DataFrame()
+    # Add all data to a dataframe
+    df = pd.DataFrame()
 
-# Find longest column
-# This will be the length of the entire dataframe
-longest_index = 0
-longest = 0
-for i in range(len(all_data)):
-    if len(all_data[i]) > longest:
-        longest_index = i
-        longest = len(all_data[i])
-times = [all_data[longest_index][i][0] for i in range(len(all_data[longest_index]))]
-df["Time"] = times
+    # Find longest column
+    # This will be the length of the entire dataframe
+    longest_index = 0
+    longest = 0
+    for i in range(len(all_data)):
+        if len(all_data[i]) > longest:
+            longest_index = i
+            longest = len(all_data[i])
+    times = [all_data[longest_index][i][0] for i in range(len(all_data[longest_index]))]
+    df["Time"] = times
 
-# Add all columns to dataframe
-for i in range(len(all_data)):
-    metric_vals = [all_data[i][j][1] for j in range(len(all_data[i]))]
-    while len(metric_vals) < longest:
-        metric_vals.append(np.NaN)
-    df[all_colnames[i]] = metric_vals
+    # Add all columns to dataframe
+    for i in range(len(all_data)):
+        metric_vals = [all_data[i][j][1] for j in range(len(all_data[i]))]
+        while len(metric_vals) < longest:
+            metric_vals.append(np.NaN)
+        df[all_colnames[i]] = metric_vals
 
-df.to_csv('SASA/sasatph.csv')
+    # DATA JUMPS FROM 200-600
+    # Sorting method is faulty
 
-# DATA JUMPS FROM 200-600
-# Sorting method is faulty
+    # Determine which dataframe has more times and append the dataframes based on time column size (greater column size appended first)
+    # Source 2 has more times than source 1
+    if len(df["Time"]) > len(source1df["Time"]):
+        # Drop the time column of source 1
+        source1df.drop('Time', inplace = True, axis = 1)
+        # Merge extracted data from source 1 and source 2 so that source 2's data is first, including its time values
+        df_anya = pd.concat([df, source1df], axis = 1)
+    # Source 1 has more times than source 2    
+    else:
+        # Drop the time column of source 2
+        df.drop('Time', inplace = True, axis = 1)
+        # Merge extracted data from source 1 and source 2 so that source 1's data is first, including its time values
+        df_anya = pd.concat([source1df, df], axis = 1)
+    return df_anya
